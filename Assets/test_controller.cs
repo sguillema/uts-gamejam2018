809e64665f0bfdc709a63e66f3f569b20f2d9d58
Sebastian Guillema <sguillema@hotmail.com>
2018-11-13 06:24:06 +1100

    Basic interaction functionality

---
+++ Assets/test_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test_controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		Debug.Log("You clicked me");	
+		SceneManager.LoadScene("Sandbox");
+	}
+}
