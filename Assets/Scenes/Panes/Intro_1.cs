140ac18ad5c99627edfd701ed69f6b27092700b3
Sebastian Guillema <sguillema@hotmail.com>
2018-11-13 13:29:48 +1100

    Intro Scene Completed

---
+++ Assets/Scenes/Panes/Intro_1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_1 : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Intro_1_scene.leftLeverActive && Intro_1_scene.rightLeverActive) {
+			Intro_1_scene.completed = true;
+		}
+		if (Global.POI1Found) {
+			Intro_1_scene.door.SetActive(false);
+		}
+	}
+
+	void OnMouseDown() {
+		if (gameObject.name == Intro_1_scene.leftLever.name && !Intro_1_scene.leftLeverActive) {
+			Debug.Log("You clicked the left lever!");
+			Intro_1_scene.leftLever.GetComponent<SpriteRenderer>().sprite = Resources.LoadAll<Sprite>("0-LeverOn")[0];
+			Intro_1_scene.leftLeverActive = true;
+		}
+		if (gameObject.name == Intro_1_scene.rightLever.name && !Intro_1_scene.rightLeverActive) {
+			Debug.Log("You clicked the right lever!");
+			Intro_1_scene.rightLever.GetComponent<SpriteRenderer>().sprite = Resources.LoadAll<Sprite>("0-LeverOn")[0];
+			Intro_1_scene.rightLeverActive = true;
+		}
+		if (gameObject.name == Intro_1_scene.door.name && Intro_1_scene.rightLeverActive && Intro_1_scene.leftLeverActive && Intro_1_scene.completed) {
+			Debug.Log("You clicked the door!");
+			Global.POI1Found = true;
+		}
+	}
+}
---
+++ Assets/Scenes/Panes/Intro_1_scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Intro_1_scene{
+	public static GameObject door = GameObject.Find("podDoor");
+	public static GameObject leftLever = GameObject.Find("lever_l");
+	public static GameObject rightLever = GameObject.Find("lever_r");
+
+	public static bool leftLeverActive = false;
+	public static bool rightLeverActive = false;
+
+	public static bool completed = false;
+}
