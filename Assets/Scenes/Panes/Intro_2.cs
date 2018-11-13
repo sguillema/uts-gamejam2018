0102810a6021ddd0327a14467a30d1bf52ca38ac
Sebastian Guillema <sguillema@hotmail.com>
2018-11-13 15:13:19 +1100

    Intro 2 WIP

---
+++ Assets/Scenes/Panes/Intro_2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		if (gameObject.name == Intro_2_scene.suit.name && !Intro_2_scene.suitTaken) {
+			Debug.Log("You took the suit");
+			Intro_2_scene.suit.SetActive(false);
+			Intro_2_scene.suitTaken = true;
+		} else if (gameObject.name == Intro_2_scene.device.name && !Intro_2_scene.deviceTaken && Intro_2_scene.suitTaken) {
+			Debug.Log("You took the device");
+			Intro_2_scene.device.SetActive(false);
+			Intro_2_scene.deviceTaken= true;
+			Global.POI2Found = true;
+		}
+		// if (gameObject.name == Intro_2_scene.suit.name) {
+		// 	Debug.Log("You took the suit");
+		// }
+	}
+}
---
+++ Assets/Scenes/Panes/Intro_2_scene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Intro_2_scene {
+	public static GameObject suit = GameObject.Find("suit");
+	public static GameObject device = GameObject.Find("device");
+
+	public static bool suitTaken = false;
+	public static bool deviceTaken = false;
+
+	public static bool completed = false;
+}
